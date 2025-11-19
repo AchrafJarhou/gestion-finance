42992d3eb413d1f4eb6ec70944ce15904c6bb855
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-11-19 10:41:57 +0100

    ajourt navbar dans la page budjets

--- app/budjets/page.tsx
+++ app/budjets/page.tsx
@@ -1,10 +1,9 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  return <Navbar />;
+};
 
-export default page
+export default page;
