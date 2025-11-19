7f4bab42dc34d4f72d675480a5545a1320fb8e7d
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-11-19 11:40:13 +0100

    ajout dun composant modal depuis daisy ui pour crée des budget

--- app/action.ts
+++ app/action.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import prisma from "@/lib/prisma";
-
+// Ajouter un utilisateur s'il n'existe pas deja
 export async function checkAndAddUser(email: string | undefined) {
   if (!email) {
     return;
@@ -14,11 +14,38 @@ export async function checkAndAddUser(email: string | undefined) {
       await prisma.user.create({
         data: { email },
       });
-        console.log("Nouvel utilisateur ajoute  :", email);
+      console.log("Nouvel utilisateur ajoute  :", email);
     } else {
       console.log("Utilisateur existe deja :", existingUser);
     }
   } catch (error) {
     console.error("Erreur lors de la verification de lutilisateur  :", error);
+  }
+}
+// Ajouter un budget pour un utilisateur
+export async function addBudget(
+  email: string,
+  name: string,
+  amount: number,
+  selectedEmoji: string
+) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      throw new Error("Utilisateur non trouve");
+    }
+    await prisma.budget.create({
+      data: {
+        name,
+        amount,
+        emoji: selectedEmoji,
+        userId: user.id,
+      },
+    });
+  } catch (error) {
+    console.error("Erreur lors de l'ajout du budget :", error);
+    throw error;
   }
 }
--- app/budjets/page.tsx
+++ app/budjets/page.tsx
@@ -1,13 +1,36 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Wrapper from "../components/Wrapper";
+import { useUser } from "@clerk/nextjs";
 
 const page = () => {
+  const { user } = useUser();
+  const [budgetName, setBudgetName] = useState<string>("");
+  const [budgetAmount, setBudgetAmount] = useState<string>("");
+  const [selectedEmoji, setSelectedEmoji] = useState("💰");
   return (
     <Wrapper>
-      <div>
-        <h1>Page des budjets</h1>
-      </div>
+      <button
+        className="btn"
+        onClick={() =>
+          (
+            document.getElementById("my_modal_3") as HTMLDialogElement
+          ).showModal()
+        }
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        </div>
+      </dialog>
     </Wrapper>
   );
 };
