9bb9f15aade62cf7a6e4efb2bb323d7678baded3
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2025-11-19 14:23:34 +0100

    mise en place de la fonctionalité de creation d'un budget

--- app/budjets/page.tsx
+++ app/budjets/page.tsx
@@ -3,12 +3,39 @@ import React, { useState } from "react";
 import Wrapper from "../components/Wrapper";
 import { useUser } from "@clerk/nextjs";
 import EmojiPicker from "emoji-picker-react";
+import prisma from "@/lib/prisma";
+import { addBudget } from "../action";
 
 const page = () => {
   const { user } = useUser();
   const [budgetName, setBudgetName] = useState<string>("");
   const [budgetAmount, setBudgetAmount] = useState<string>("");
-  const [selectedEmoji, setSelectedEmoji] = useState("💰");
+  const [selectedEmoji, setSelectedEmoji] = useState("");
+  const [showEmojiPicker, setShowEmojiPicker] = useState<boolean>(false);
+  const handleEmojiSelect = (emojiObject: { emoji: string }) => {
+    setSelectedEmoji(emojiObject.emoji);
+    setShowEmojiPicker(false);
+  };
+  const handleAddBudget = async () => {
+    try {
+      const amount = parseFloat(budgetAmount);
+      if (isNaN(amount) || amount <= 0) {
+        throw new Error("Montant doit etre un nombre positif");
+      }
+      await addBudget(
+        user?.primaryEmailAddress?.emailAddress as string,
+        budgetName,
+        amount,
+        selectedEmoji
+      );
+      setBudgetName("");
+      setBudgetAmount("");
+      setSelectedEmoji("");
+      (document.getElementById("my_modal_3") as HTMLDialogElement).close();
+    } catch (error) {
+      console.error("Erreur lors de l'ajout du budget :", error);
+    }
+  };
   return (
     <Wrapper>
       <button
@@ -36,7 +63,7 @@ const page = () => {
               value={budgetName}
               placeholder="Nom du budget"
               onChange={(e) => setBudgetName(e.target.value)}
-              className="input input-bordered mb-3"
+              className="input input-bordered w-full mb-3"
               required
             />
             <input
@@ -44,11 +71,23 @@ const page = () => {
               value={budgetAmount}
               placeholder="montant du budget"
               onChange={(e) => setBudgetAmount(e.target.value)}
-              className="input input-bordered mb-3"
+              className="input input-bordered w-full mb-3"
               required
             />
-            <EmojiPicker />
-            <button className="btn btn-accent">Ajouter budget</button>
+            <button
+              onClick={() => setShowEmojiPicker(!showEmojiPicker)}
+              className="btn btn-ghost mb-3"
+            >
+              {selectedEmoji || "selectionez un emoji"}
+            </button>
+            <div className="flex justify-center items-center my-4">
+              {showEmojiPicker && (
+                <EmojiPicker onEmojiClick={handleEmojiSelect} />
+              )}
+            </div>
+            <button onClick={handleAddBudget} className="btn btn-accent">
+              Ajouter budget
+            </button>
           </div>
         </div>
       </dialog>
